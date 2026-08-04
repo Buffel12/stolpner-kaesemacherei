72918975e11e78e2997a4d22158f7584ca157cf8
Buffel12 <bud.specter@pm.me>
2026-08-04 11:56:04 +0200

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://buffel12.github.io',
+  base: '/stolpner-kaesemacherei',
   // Domain aus der Website-Architektur (stolpner-kaesemacherei.de)
   site: 'https://stolpner-kaesemacherei.de',
 });
