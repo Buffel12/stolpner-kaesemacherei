6dd4e00162eb6e3f63d7dee24a8b6ea4674f842e
Buffel12 <bud.specter@pm.me>
2026-08-04 12:01:26 +0200

    Update site URL in astro.config.mjs
    Removed old site URL from configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://buffel12.github.io',
   base: '/stolpner-kaesemacherei',
-  // Domain aus der Website-Architektur (stolpner-kaesemacherei.de)
-  site: 'https://stolpner-kaesemacherei.de',
 });
